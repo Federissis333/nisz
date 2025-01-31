b1c77da2597ac8232fe03493cc9b39208de051d5
Federissis333 <110787619+Federissis333@users.noreply.github.com>
2025-01-31 03:26:07 -0300

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     userId: { type: String, required: true, unique: true },
-    username: { type: String, required: true },
-    balance: { type: Number, default: 0 },
-    investment: { type: Number, default: 0 },
-    bank: { type: Number, default: 0 }
-});
+        username: { type: String, required: true },
+            balance: { type: Number, default: 0 },
+                investment: { type: Number, default: 0 },
+                    bank: { type: Number, default: 0 },
+                        fundo: { type: String, default: 'default.png' } // Fundo do perfil
+                        });
 
-const User = mongoose.model('User', userSchema);
+                        const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+                        module.exports = User;
+                        
